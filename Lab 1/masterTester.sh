7fe42235853655ff6c07181a12807cc72eb97d55
CodeGenki <genoji37@gmail.com>
2017-09-16 23:32:19 -0500

    Improved testing script

--- Lab 1/masterTester.sh
+++ Lab 1/masterTester.sh
@@ -4,23 +4,23 @@ echo "Testing error code 1..."
 for f in ./TestFiles/1/*.txt
 do
   ./assemble $f output.obj
-  echo "$?"
+  echo "$f: $?"
 done
 echo "Testing error code 2..."
 for f in ./TestFiles/2/*.txt
 do
   ./assemble $f output.obj
-  echo "$?"
+  echo "$f: $?"
 done
 echo "Testing error code 3..."
 for f in ./TestFiles/3/*.txt
 do
   ./assemble $f output.obj
-  echo "$?"
+  echo "$f: $?"
 done
 echo "Testing error code 4..."
 for f in ./TestFiles/4/*.txt
 do
   ./assemble $f output.obj
-  echo "$?"
+  echo "$f: $?"
 done
